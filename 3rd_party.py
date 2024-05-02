50c6de0de03431f8692ea2a0e011607a36d4de3a
dabeersboys <68974202+dabeersboys@users.noreply.github.com>
2024-05-02 13:29:39 -0600

    Add files via upload

---
+++ 3rd_party.py
@@ -0,0 +1,12 @@
+def filter_lines(input_file, output_file):
+    with open(input_file, 'r') as infile:
+        lines = infile.readlines()
+
+    filtered_lines = [line for line in lines if not line.startswith(('com.apple', 'com.android', 'com.google', 'com.samsung', 'com.mediatek', 'com.sec.android', 'android.auto'))]
+
+    with open(output_file, 'w') as outfile:
+        outfile.writelines(filtered_lines)
+
+input_file = input("Drag and Drop Installed Apps Text File: ").strip('"')  # Removing surrounding quotes from the file path
+output_file = "./Possible_3rdParty_apps.txt"
+filter_lines(input_file, output_file)
